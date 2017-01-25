2193b00464434d381e39bf1bd448a75837e50f3f
edgarallanglez <edgarallanglez@gmail.com>
2017-01-24 19:00:41 -0700

    backend architecture

---
+++ project_x/config/config.js
@@ -0,0 +1,34 @@
+var path = require('path'),
+    rootPath = path.normalize(__dirname + '/..'),
+    env = process.env.NODE_ENV || 'development';
+
+var config = {
+  development: {
+    root: rootPath,
+    app: {
+      name: 'project-x'
+    },
+    port: process.env.PORT || 3000,
+    db: 'postgres://localhost/project-x-development'
+  },
+
+  test: {
+    root: rootPath,
+    app: {
+      name: 'project-x'
+    },
+    port: process.env.PORT || 3000,
+    db: 'postgres://localhost/project-x-test'
+  },
+
+  production: {
+    root: rootPath,
+    app: {
+      name: 'project-x'
+    },
+    port: process.env.PORT || 3000,
+    db: 'postgres://localhost/project-x-production'
+  }
+};
+
+module.exports = config[env];
